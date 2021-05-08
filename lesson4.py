6a28ee0b91490e3fc33b5fbea2b1da316ca53124
Nurbek <nurbekkalilov@gmail.com>
2021-05-08 19:48:48 +0600

    New commit

---
+++ lesson4.py
@@ -0,0 +1,37 @@
+# list = ['Cheese', 'Ketchup', 'Bread', 'Water']
+#
+# print(list[-3:
+
+
+# list = ('Cheese', 'Ketchup', 'Bread', 'Water')
+#print(list(range(0, 10)))
+#
+# list = ['Cheese', 'Ketchup', 'Bread', 'Water']
+# for i in list:
+#     print(f'List {i}')
+
+# winning_ticket = 77
+# lottery_process = True
+# #lottery_process = False
+#
+# while lottery_process:
+#     lucky_number = int(input('Enter the number:'))
+#     if winning_ticket == lucky_number:
+#         print('You won the lottery')
+#
+#     else:
+#         print('Unfortunately you lose')
+#
+# else:
+#     print('lottery process in not available')
+
+d = 0
+
+while d < 10:
+    print(d)
+    d += 1
+
+
+
+
+
