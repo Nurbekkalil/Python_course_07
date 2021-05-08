51e5ee7677f7a202d1057d4d000ab468376146e4
Nurbekkalil <83503611+Nurbekkalil@users.noreply.github.com>
2021-05-08 11:55:03 +0600

    Update third_lesson_hwork.py

---
+++ third_lesson_hwork.py
@@ -0,0 +1,27 @@
+int_1 = int(input())
+int_2 = int(input())
+int_3 = int(input())
+
+result = int_1 + int_2 + int_3
+
+print(result)
+
+#################
+
+
+#Lottery Game
+
+winning_ticket = 77
+lottery_process = True
+#lottery_process = False
+
+while lottery_process:
+    lucky_number = int(input('Enter the number:'))
+    if winning_ticket == lucky_number:
+        print('You won the lottery')
+
+    else:
+        print('Unfortunately you lose')
+
+else:
+    print('lottery process in not available')
