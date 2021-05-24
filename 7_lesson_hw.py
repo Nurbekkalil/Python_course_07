ed7d10027e2990156a5d50d87926024ca6970aac
Nurbekkalil <83503611+Nurbekkalil@users.noreply.github.com>
2021-05-24 18:49:42 +0600

    Update 7_lesson_hw.py

--- 7_lesson_hw.py
+++ 7_lesson_hw.py
@@ -1,11 +1,1 @@
-def take_students(num):
-    l = []
-    for i in range(num):
-        student = input()
-        if student == "Daniyar":
-            return 'teacher can not be student'
-        l.append(student)
 
-    return l
-
-print(take_students(3))
